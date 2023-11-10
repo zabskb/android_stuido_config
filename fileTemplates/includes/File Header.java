f966c42330515ae69df90420f29243a97d3981a6
bynav <bynav@xuwei>
2023-11-10 09:56:11 +0800

    AI-Giraffe | 2022.3.1 <bynav@xuwei Create baseRefactoring.xml, ide.general.xml, _@user_Default.icls, project.default.xml, diff.xml, Enum.java, Interface.java, gradle.experimental.xml, Default.xml, androidStudioFirstRun.xml, find.xml, ui.lnf.xml, _@user_Material Sandy Beach.icls, File Header.java, filetypes.xml, androidEditors.xml, editor-font.xml, New Kotlin Function Body.kt, AndroidLog.xml, colors.scheme.xml, kotlinRefactoring.xml, notifications.xml, _@user_Darcula.icls, AndroidLogKotlin.xml, androidLogcatFilterHistory.xml, _@user_IntelliJ Light.icls, templates.xml, Record.java, laf.xml, androidLogcatFormattingOptions.xml, customization.xml, Default.xml, androidLogcatSettings.xml, _@user_Dark.icls, _@user_Material Darker.icls, _@user_Material Palenight.icls, AndroidXML.xml, Class.java, jdk.table.xml, proxy.settings.xml, encoding.xml, vcs.xml, _@user_Solarized Light _Material_.icls, editor.xml, rainbow_brackets_lite.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,7 @@
+/**
+* @ClassName: ${NAME}
+* @Description: 
+* @Author: xuwei
+* @Date: ${DATE} ${TIME}
+* Copyright (C), 2013-${YEAR}, 湖南北云科技有限公司
+*/
---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* @ClassName: ${NAME}
+* @Description: 
+* @Author: xuwei
+* @Date: ${DATE} ${TIME}
+* Copyright (C), 2013-${YEAR}, 湖南北云科技有限公司
+*/
+public enum ${NAME} {
+}
